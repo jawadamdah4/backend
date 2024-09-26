d241ed0ea336d1b494d4f0e5f94dbed919724190
jawadamdah4 <jawadamdah434@gmail.com>
2024-09-26 11:02:38 +0100

    first commit

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const prot = 8000 || process.env.PORT 
-app.get('/', (req, res) => {
-    res.send('Welcome! Home Page.')
-})
-app.post('/NewLicense', (req, res) => {
-    console.log(req.body)
-    res.sendStatus(201)
-})
-app.get('/CheckMyKey', (req, res) => {
-    const body = req.body
-    if(body == "Amdah")
-    res.send('You Are Conneted!').sendStatus(201)
-else
-res.send('Your code is wormg!').sendStatus(404)
-})
+const express = require("express");
+const app = express();
+app.use(express.json());
+const prot = 8000 || process.env.PORT;
+app.get("/", (req, res) => {
+  res.send("Welcome! Home Page.");
+});
+app.post("/NewLicense", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(201);
+});
+app.get("/CheckMyKey", (req, res) => {
+  const body = req.body;
+  if (body == "Amdah") res.send("You Are Conneted!").sendStatus(201);
+  else res.send("Your code is wormg!").sendStatus(404);
+});
 
 app.listen(prot, () => {
-    console.log("app hosted succsusfully on ${port}")
-})
+  console.log("app hosted succsusfully on ${port}");
+});
